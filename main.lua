67e8934ca83ed951d0856ed2513ed75acb86335a
Daniel Kao <daniel.kao@gmail.com>
2023-07-08 20:34:51 +0800

    feat: could be set up in dictionary list, when clicking on word, it will look up automatically.

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
 local android = require("android")
+local Device = require("device")
 local InputContainer = require("ui/widget/container/inputcontainer")
 local _ = require("gettext")
 
@@ -7,16 +8,54 @@ local AskGPT = InputContainer:new {
   is_doc_only = true,
 }
 
+local external = require("device/thirdparty"):new{
+  dicts = {
+      { "Aard2", "Aard2", false, "itkach.aard2", "aard2" },
+      { "Alpus", "Alpus", false, "com.ngcomputing.fora.android", "search" },
+      { "ColorDict", "ColorDict", false, "com.socialnmobile.colordict", "send" },
+      { "Eudic", "Eudic", false, "com.eusoft.eudic", "send" },
+      { "EudicPlay", "Eudic (Google Play)", false, "com.qianyan.eudic", "send" },
+      { "Fora", "Fora Dict", false, "com.ngc.fora", "search" },
+      { "ForaPro", "Fora Dict Pro", false, "com.ngc.fora.android", "search" },
+      { "GoldenFree", "GoldenDict Free", false, "mobi.goldendict.android.free", "send" },
+      { "GoldenPro", "GoldenDict Pro", false, "mobi.goldendict.android", "send" },
+      { "Kiwix", "Kiwix", false, "org.kiwix.kiwixmobile", "text" },
+      { "LookUp", "Look Up", false, "gaurav.lookup", "send" },
+      { "LookUpPro", "Look Up Pro", false, "gaurav.lookuppro", "send" },
+      { "Mdict", "Mdict", false, "cn.mdict", "send" },
+      { "QuickDic", "QuickDic", false, "de.reimardoeffinger.quickdic", "quickdic" },
+      { "EinkBro", "EinkBro", false, "info.plateaukao.einkbro", "text" },
+  },
+  check = function(self, app)
+      return android.isPackageEnabled(app)
+  end,
+}
+
+local function getExternalDictLookupList(self) 
+  return external.dicts 
+end
+
+local doExternalDictLookup = function (self, text, method, callback)
+  external.when_back_callback = callback
+  local _, app, action = external:checkMethod("dict", method)
+  if action then
+      android.dictLookup(text, app, action)
+  end
+end
+
 function AskGPT:init()
-  self.ui.highlight:addToHighlightDialog("askeinkbro", function(_reader_highlight_instance)
+  self.ui.highlight:addToHighlightDialog("askeinkbro", function(this)
     return {
       text = _("Query EinkBro"),
       enabled = yes,
       callback = function()
-        android.dictLookup(_reader_highlight_instance.selected_text.text, "info.plateaukao.einkbro", "send")
+        android.dictLookup(this.selected_text.text, "info.plateaukao.einkbro", "text")
+        this:onClose()
       end,
     }
   end)
+  Device.getExternalDictLookupList = getExternalDictLookupList
+  Device.doExternalDictLookup = doExternalDictLookup
 end
 
 return AskGPT
